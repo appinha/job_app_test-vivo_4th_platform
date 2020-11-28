e59c5cd3a555d31f942d2ebd66d5fc07593b583e
appinha <amanda_pinha@hotmail.com>
2020-11-28 17:40:11 -0300

    Ex.1: commented code; refined testing

--- ex1/solution.py
+++ ex1/solution.py
@@ -11,8 +11,14 @@
 import numpy as np
 
 def count_arr_in_matrix(array, matrix):
+	"""
+		This function takes in a 1D array and a 2D array (matrix). It counts
+		how many times each array element occurs in the matrix. The return is a
+		space-comma string with occurrence counts.
+	"""
 	res = []
 
+	# Check input conditions, i.e. if elements are in range(16).
 	if np.all((matrix >= 0) & (matrix <= 15)) and \
 		np.all((array >= 0) & (array <= 15)) :
 		for i in range(len(array)):
--- ex1/testing.py
+++ ex1/testing.py
@@ -3,34 +3,45 @@ import numpy as np
 from solution import *
 
 
-def test(i, M, N, a, low, high):
+def test(i, M, N, a, low, high, v=False):
 	random_matrix = np.random.randint(low, high, (M, N))
 	random_array = np.random.randint(low, high, (1, a))
 	control_array = np.arange(high - low)
-	control_res = ""
-
-	#print(colored("\nInputs:", 'blue'))
-	#print(random_matrix)
-	#print(random_array[0])
-	#print(colored("Answer:", 'blue'), count_arr_in_matrix(random_array[0], random_matrix))
 	control_res = count_arr_in_matrix(control_array, random_matrix)
-	if (control_res and sum(list(map(int, control_res.split(', ')))) == random_matrix.size):
-		print(colored("Test", 'green'), colored(i, 'green'), colored(":", 'green'), "passed")
+	s = "Test {:>2}:".format(i)
+	if control_res:
+		control_sum = sum(list(map(int, control_res.split(', '))))
+		if control_sum == random_matrix.size:
+			print(colored(s, 'green'), "passed")
+		else:
+			print(colored(s, 'red'), "failed")
 	else:
-		print(colored("Test", 'red'), colored(i, 'red'), colored(":", 'red'), "failed")
+		print(colored(s, 'red'), "invalid input")
+	if v:
+		print(colored(" • Inputs:", 'blue'))
+		print(random_matrix)
+		print(random_array[0])
+		print(colored(" • Answer:", 'blue'), count_arr_in_matrix(
+				random_array[0], random_matrix))
+		if control_sum:
+			print(colored(" • Control:", 'yellow'),
+					"{} vs {} (sum of counts vs qty of matrix elements)\n\
+					".format(control_sum, random_matrix.size))
 
-
+low = 0
+high = 16
 a = 3
 i = 1
 
+test(i, 2, 5, a, low, high, v=True)
+i += 1
+
 for N in range (2, 5):
 	for M in range (2,5):
-		test(i, M, N, a, 0, 16)
+		test(i, M, N, a, low, high)
 		i += 1
 
 for N in range (2, 4):
 	for M in range (2,4):
 		test(i, 2, 5, a, 16, 20)
 		i += 1
-
-print("")
