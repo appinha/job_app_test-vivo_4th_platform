0661b7fd5eaf4d729cfb3c1720fc527d36d5bf8d
appinha <amanda_pinha@hotmail.com>
2020-12-02 16:13:03 -0300

    Ex.2: optimized run_app.sh script

--- ex2/run/run_app.sh
+++ ex2/run/run_app.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# Check requirements
-check=$(pip freeze | grep 'Flask\|numpy')
-[[ $check != *"Flask=="* ]] && pip install Flask
-[[ $check != *"Flask-RESTful=="* ]] && pip install flask-restful
-[[ $check != *"numpy=="* ]] && pip install numpy
+## Script for running in venv
 
+# Check requirements
+check=$(pip freeze)
+requirements='requirements.txt'
+while read line; do
+	[[ $check != *"$line"* ]] && pip install $line
+done < $requirements
+
+# Run Flask application
 export FLASK_APP=app.py
 [[ $1 == "dev" ]] && export FLASK_ENV=development
 flask run
